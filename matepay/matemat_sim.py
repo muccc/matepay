a126efe02300bb93824aaf26b74bafdd86586b80
Tobias Schneider <schneider@blinkenlichts.net>
2014-03-22 23:59:52 +0100

    fix(matemat_sim): use standard python logging

--- matepay/matemat_sim.py
+++ matepay/matemat_sim.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python
 
 import sys
-from upay.logger import getLogger, flogger
+import logging
 
 class Matemat:
-    log = getLogger('MatematSim')
-
     def __init__(self):
+        self._logger = logging.getLogger(__name__)
         pass
 
-    @flogger(log)
     def writeLCD(self, msg):
-        self.log.info('writeLCD(): msg=%s' % msg)
+        self._logger.info('writeLCD(): msg=%s' % msg)
     
-    @flogger(log)
     def getPriceline(self):
         while True:
             try:
                 print 'Priceline?'
                 p = int(sys.stdin.readline().strip())
-                self.log.debug('priceline=%s' % p)
+                self._logger.debug('priceline=%s' % p)
                 return p
             except ValueError:
                 pass
 
-    @flogger(log)
     def serve(self,priceline):
-        self.log.info('priceline=%s' % priceline)
+        self._logger.info('priceline=%s' % priceline)
         # always serve
         return True
 
-    @flogger(log)
     def completeserve(self):
         return True
 
