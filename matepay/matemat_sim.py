63a27c9f8c693de1024656acd354dd67cfc29138
Tobias Schneider <schneider@blinkenlichts.net>
2014-03-23 06:02:11 +0100

    fix(matemat-sim): fix the simulator

--- matepay/matemat_sim.py
+++ matepay/matemat_sim.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-
 import sys
 import logging
+from decimal import Decimal
 
 class Matemat:
     def __init__(self):
@@ -11,30 +11,13 @@ class Matemat:
     def writeLCD(self, msg):
         self._logger.info('writeLCD(): msg=%s' % msg)
     
-    def getPriceline(self):
-        while True:
-            try:
-                print 'Priceline?'
-                p = int(sys.stdin.readline().strip())
-                self._logger.debug('priceline=%s' % p)
-                return p
-            except ValueError:
-                pass
+    def getCost(self):
+        print 'cost?'
+        return Decimal(sys.stdin.readline().strip())
 
-    def serve(self,priceline):
-        self._logger.info('priceline=%s' % priceline)
-        # always serve
+    def serve(self):
         return True
 
     def completeserve(self):
         return True
-
-# "Testing"
-if __name__ == '__main__':
-    m = MatematSim()
-    m.writeLCD("luvv")
-    while m.getPriceline() != 3:
-        time.sleep(0.2)
-    m.serve(3)
-    m.completeserve()
 
